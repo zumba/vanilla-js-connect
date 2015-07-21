78b30995b169e09d2d032d68c0f7727e7940f185
Jonathan Feng <jfeng13@uchicago.edu>
2015-07-21 15:58:45 -0400

    linted

--- src/ValidatorInterface.php
+++ src/ValidatorInterface.php
@@ -4,6 +4,13 @@ namespace Zumba\VanillaJsConnect;
 
 interface ValidatorInterface
 {
-
+    /**
+     * Interface for validators. Returns a Response object or nothing
+     *
+     * @param  Request $request
+     * @param  User  $user
+     * @param  Config  $config
+     * @return Response           
+     */
     public function validate(Request $request, User $user = null, Config $config = null);
 }
