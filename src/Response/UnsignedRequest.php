6fcd35d83dc27d60bf2cf49aac46973016545719
Pablo Molina <web.kod3r@gmail.com>
2020-04-30 15:26:34 -0400

    Updating unsigned request

---
+++ src/Response/UnsignedRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Zumba\VanillaJsConnect\Response;
+
+use Zumba\VanillaJsConnect\Config;
+use Zumba\VanillaJsConnect\Contracts\VanillaUser;
+use Zumba\VanillaJsConnect\Request;
+use Zumba\VanillaJsConnect\Response;
+
+class UnsignedRequest extends Response
+{
+    /**
+     * Boolean whether the user is signed in
+     *
+     * @var boolean
+     */
+    protected $signedIn;
+
+    /**
+     * Sets variables. Default is ''
+     *
+     * @param Request $request
+     * @param VanillaUser $user
+     * @param Config $config
+     */
+    public function __construct(Request $request, VanillaUser $user, Config $config)
+    {
+        parent::__construct($request, $user, $config);
+    }
+
+    /**
+     * Sets signedin key in array if true
+     *
+     * @return boolean
+     */
+    private function isSignedIn()
+    {
+        return !empty($this->user) && !empty($this->user->getUid());
+    }
+}
